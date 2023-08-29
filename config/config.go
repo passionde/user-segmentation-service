ad677c8bc6eac5707d40a4ad4a08bd76491bd043
passionde <yarikdev@bk.ru>
2023-08-29 20:33:38 +0500

    <FIX>: Removed URL to connect to RabbitMQ

--- config/config.go
+++ config/config.go
@@ -12,7 +12,6 @@ type (
 		HTTP   `yaml:"http"`
 		Log    `yaml:"log"`
 		PG     `yaml:"postgres"`
-		AMQP   `yaml:"amqp"`
 		Secure `yaml:"secure"`
 	}
 
@@ -32,10 +31,6 @@ type (
 	PG struct {
 		MaxPoolSize int    `env-required:"true" yaml:"max_pool_size" env:"PG_MAX_POOL_SIZE"`
 		URL         string `env-required:"true"                      env:"PG_URL"`
-	}
-
-	AMQP struct {
-		URL string `env-required:"true"                      env:"AMQP_URL"`
 	}
 
 	Secure struct {
